448afb59308af6dbefebd391a42dc6e427533abf
CarsonMaurer <111007062+CarsonMaurer@users.noreply.github.com>
2022-11-16 11:01:39 -0800

    difficulty

--- 2d user interface/Assets/Scripts/Target.cs
+++ 2d user interface/Assets/Scripts/Target.cs
@@ -9,12 +9,15 @@ public class Target : MonoBehaviour
     public float MaxSpeed = 20;
     public float MaxTorque = 10;
     private Rigidbody2D _targetRb;
+     private GameManager _gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
         _targetRb = GetComponent<Rigidbody2D>();
         _targetRb.AddForce(Vector2.up * RandomizeForce(), ForceMode2D.Impulse);
         _targetRb.AddTorque(RandomizeTorque());
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -29,5 +32,23 @@ public class Target : MonoBehaviour
     private float RandomizeTorque()
     {
         return Random.Range(-MaxTorque, MaxTorque);
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("You Clcked on " + gameObject.name);
+        _gameManager.UpdateScore(PointValue);
+        Destroy(this.gameObject);
+
+    }
+       private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+
+        if(!gameObject.CompareTag("bad"))
+        {
+            //Debug.Log("Game Over");
+            _gameManager.GameOver();
+            
+        }
     }
 }
